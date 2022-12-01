d93eac46463533d9fb94c8fcd5fd3ede1045f937
Pablo <pablomunozgarcia@outlook.com>
2022-12-01 18:43:44 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,38 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    data class Elf(val calories: Int)
+
+    fun parseElves(input: List<String>): List<Elf> {
+        val elves = mutableListOf<Elf>()
+        var currentElf = Elf(0)
+
+        input.forEach {
+            when {
+                it.isEmpty() -> elves.add(currentElf.copy()).also { currentElf = Elf(0) }
+                else -> currentElf = currentElf.copy(calories = currentElf.calories + it.toInt())
+            }
+        }
+
+        elves.add(currentElf)
+
+        return elves
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int =
+        parseElves(input).maxBy { it.calories }.calories
+
+
+    fun part2(input: List<String>): Int =
+        parseElves(input).sortedByDescending { it.calories }.take(3).sumOf { it.calories }
+
 
     // test if implementation meets criteria from the description, like:
+
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
+    println(part2(testInput))
     println(part2(input))
 }
